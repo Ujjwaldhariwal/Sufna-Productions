55ed93ac3067f1c10f999b6d82f8889d2c238e27
Ujjwal Dhariwal <ujjwaldh@infinite.com>
2024-06-18 14:19:21 +0530

    last page left only

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -44,7 +44,7 @@ const Contact = () => {
       transition={transition1}
       className='section bg-white'
     >
-      
+
       <div className='container mx-auto h-full'>
         <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-32 gap-x-8 text-center lg:text-left'>
           {/* bg */}
@@ -61,7 +61,7 @@ const Contact = () => {
             onMouseLeave={mouseLeaveHandler}
             className='lg:flex-1 lg:pt-32 px-4'
           >
-            
+
             <h1 className='h1'>Contact Us</h1>
             {result === "Form Submitted Successfully" ? (
               <div className='flex flex-col gap-y-4 lg:items-center lg:justify-center'>
@@ -97,12 +97,18 @@ const Contact = () => {
                 </button>
               </form>
             )}
-            
+
 
             <div className='flex justify-center lg:hidden gap-x-4 mt-4'>
-              <FaFacebookF className='social-icon1' />
-              <FaInstagram className='social-icon2' />
-              <FaYoutube className='social-icon3' />
+              <a href="https://www.facebook.com/people/Sufna-Productions/61550937880697/?mibextid=LQQJ4d">
+                <FaFacebookF className='social-icon1' />
+              </a>
+              <a href="https://www.instagram.com/sufna.productions/">
+                <FaInstagram className='social-icon2' />
+              </a>
+              <a href="https://www.youtube.com/@Sufna.Productions">
+                <FaYoutube className='social-icon3' />
+              </a>
             </div>
             <span>{result !== "Form Submitted Successfully" && result}</span>
           </div>
