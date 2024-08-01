64f59083c33d17f5860c7f0899731bcda96fb391
Ujjwal <ujjwaldhariwal0@gmail.com>
2024-08-02 02:43:56 +0530

    Revert "-changes (#1)" (#2)
    This reverts commit becf358b3467867e73ce77a6cec68e36b465b17a.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,46 +1,42 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom"; // Import Link for internal navigation
-import { motion } from "framer-motion";
-import { transition1 } from "../transitions";
-import { CursorContext } from "../context/CursorContext";
-import "./Portfolio.css";
-import "./btn.css";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for internal navigation
+import { motion } from 'framer-motion';
+import { transition1 } from '../transitions';
+import { CursorContext } from '../context/CursorContext';
+import './Portfolio.css';
+import './btn.css';
 
 const Portfolio = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
 
   return (
     <motion.section
-      initial={{ opacity: 0, y: "100%" }}
+      initial={{ opacity: 0, y: '100%' }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: "100%" }}
+      exit={{ opacity: 0, y: '100%' }}
       transition={transition1}
-      className="section"
+      className='section'
     >
-      <div className="container mx-auto h-full relative">
-        <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8">
+      <div className='container mx-auto h-full relative'>
+        <div className='flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8'>
           <motion.div
             onMouseEnter={mouseEnterHandler}
             onMouseLeave={mouseLeaveHandler}
-            initial={{ opacity: 0, y: "-80%" }}
+            initial={{ opacity: 0, y: '-80%' }}
             animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: "-80%" }}
+            exit={{ opacity: 0, y: '-80%' }}
             transition={transition1}
-            className="flex flex-col lg:items-start"
+            className='flex flex-col lg:items-start'
           >
-            <h1 className="h1">Portfolio</h1>
-            <p className="mb-12 text-xl">
+            <h1 className='h1'>Portfolio</h1>
+            <p className='mb-12 max-w-sm'>
               Our portfolio showcases a diverse range of work including
-              <b style={{ color: "#0a5647" }}>
-                {" "}
-                photoshoots, ad campaigns, brand shoots
-              </b>{" "}
-              and more.
+              <b style={{ color: '#0a5647' }}> photoshoots, ad campaigns, brand shoots</b> and more.
               <br />
               <br />
             </p>
             <div className="portfolio-button-container">
-              <Link to="/image-gallery">
+              <Link to="/image-gallery" >
                 <button className="btn-abu">
                   <div>Photos</div>
                 </button>
@@ -56,34 +52,34 @@ const Portfolio = () => {
           <div
             onMouseEnter={mouseEnterHandler}
             onMouseLeave={mouseLeaveHandler}
-            className="grid grid-cols-2 lg:gap-2 hidden lg:grid"
+            className='grid grid-cols-2 lg:gap-2 hidden lg:grid'
           >
-            <div className="image-container">
+            <div className='image-container'>
               <img
-                className="portfolio-image"
+                className='portfolio-image'
                 src="https://ik.imagekit.io/UjjwalDhariwal/Sufna%20PRoductions/p1.jpg?updatedAt=1721991594640"
-                alt="1"
+                alt='1'
               />
             </div>
-            <div className="image-container">
+            <div className='image-container'>
               <img
-                className="portfolio-image"
+                className='portfolio-image'
                 src="https://ik.imagekit.io/UjjwalDhariwal/Sufna%20PRoductions/p2.jpg?updatedAt=1721991594800"
-                alt="2"
+                alt='2'
               />
             </div>
-            <div className="image-container">
+            <div className='image-container'>
               <img
-                className="portfolio-image"
+                className='portfolio-image'
                 src="https://ik.imagekit.io/UjjwalDhariwal/Sufna%20PRoductions/p3.jpg?updatedAt=1721991594768"
-                alt="3"
+                alt='3'
               />
             </div>
-            <div className="image-container">
+            <div className='image-container'>
               <img
-                className="portfolio-image"
+                className='portfolio-image'
                 src="https://ik.imagekit.io/UjjwalDhariwal/Sufna%20PRoductions/p4.jpg?updatedAt=1721991594711"
-                alt="4"
+                alt='4'
               />
             </div>
           </div>
